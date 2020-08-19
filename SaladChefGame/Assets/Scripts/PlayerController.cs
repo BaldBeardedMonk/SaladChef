cdaa745d51efba2480ca8866933cf242dea536c6
Abhineet <abhineetkrishna@gmail.com>
2020-08-19 21:48:26 +0530

    Implemented play area and character movement
    1. Implemented the Play Area. This is dynamic and will scale to the screen size of the device.
    2.Basic character movement implemented using Unity's new input manager.
    3. Local multiplayer (2 players) implemented with basic movement. Player 1 uses WASD and Player 2 uses the arrow keys.
    4. Collision system integrated to prevent the players from moving out of the play area.
    5. Use of layer collision matrix to optimize the physics performance.

---
+++ SaladChefGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 input_movement;
+
+    [SerializeField]
+    float moveSpeed = 10f;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector2 movement = new Vector2(input_movement.x, input_movement.y)*moveSpeed*Time.deltaTime;
+        transform.Translate(movement);
+    }
+
+    private void OnMove(InputValue inputValue)  //function to capture input from the new input system
+    {
+        input_movement = inputValue.Get<Vector2>();
+    }
+}
---
+++ SaladChefGame/Assets/Scripts/SetPlayArea.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Script to dynamically set the play area sprites - Stretch the background and place the borders */
+
+public class SetPlayArea : MonoBehaviour
+{
+    public Transform Background,BorderLeft, BorderRight, BorderBottom, BorderTop;
+
+    Vector2 scale, screenBounds;
+
+    private void Awake()
+    {
+        scale = SetSpriteScale(Background.GetComponent<SpriteRenderer>());
+        SetBorderSprites();
+
+        Background.localScale = scale;
+        BorderLeft.localScale = BorderRight.localScale = new Vector3(BorderLeft.localScale.x, scale.y, BorderLeft.localScale.z);
+        BorderTop.localScale = BorderBottom.localScale = new Vector3(scale.x, BorderBottom.localScale.y, BorderBottom.localScale.z);
+
+    }
+
+    /* Function to return the scale for the sprite as Vector2  to fit the entire screen*/
+    Vector2 SetSpriteScale(SpriteRenderer spriteRenderer)
+    {
+        float camHeight = Camera.main.orthographicSize * 2;
+        float camWidth = Camera.main.aspect * camHeight;
+        Vector2 cameraSize = new Vector2(camWidth, camHeight);
+        Vector2 spriteSize = spriteRenderer.sprite.bounds.size;
+
+        float scalex = transform.localScale.x;
+        float scaley = transform.localScale.y;
+        scalex *= cameraSize.x / spriteSize.x;
+        scaley *= cameraSize.y / spriteSize.y;
+
+        return new Vector2(scalex, scaley);
+
+    }
+
+    /* Function to place the border sprites to the edge of the screen*/
+    void SetBorderSprites()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
+        BorderLeft.localPosition = new Vector3(-screenBounds.x + BorderLeft.localScale.x, BorderLeft.localPosition.y, BorderLeft.localPosition.z);
+        BorderRight.localPosition = new Vector3(screenBounds.x - BorderLeft.localScale.x, BorderRight.localPosition.y, BorderRight.localPosition.z);
+        BorderBottom.localPosition = new Vector3(BorderBottom.localPosition.x, -screenBounds.y + BorderBottom.localScale.y, BorderBottom.localPosition.z);
+        BorderTop.localPosition = new Vector3(BorderTop.localPosition.x, 0.65f * screenBounds.y, BorderTop.localPosition.z);
+
+    }
+}
