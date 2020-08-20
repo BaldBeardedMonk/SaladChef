b28524f99dcd9bb789b4027830d060d7036f34b7
Abhineet <abhineetkrishna@gmail.com>
2020-08-20 21:23:02 +0530

    Implemented picking up vegetables and placing them on the copping board
    *All the implementations are extensible with slight modification to the code and the assosciated prefab. An upper limit can be decided in the future.*
    1. Placement of vegetables on the shelf - this can be dynamic and can range from 2-10
    2. Picking up of vegetables from the shelf - easily changeable to the count desired. (1,2,3 or more)
    3. Placement of vegetables on the chopping board - easily changeable to the count desired.

---
+++ SaladChefGame/Assets/Scripts/PlaceVegetables.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Script to dynamically place the vegetables on the shelfs
+ * Set the vegetable sprites size in the inspector - should be between 2-10
+ * Drag the individual vegetable sprites in the inspector */
+
+public class PlaceVegetables : MonoBehaviour
+{
+    public GameObject Vegetable;
+    public GameObject LeftBorder, RightBorder;
+    [Tooltip("Value between 2 to 10 ")]
+    public Sprite[] VegetableSprites;
+    Sprite tempSprite;
+    float distance;
+
+    void Start()
+    {
+        distance = VegetableSprites[0].bounds.size.y * 2f / 10f;    // Distance between vegetables placed on the shelf - calculated based on the sprite size and the local scale
+        Shuffle();                                                  // This creates variation in the game where the order of vegetables on the shelf changes in every play
+        PlaceVegetableOnShelf(LeftBorder,0, VegetableSprites.Length / 2);
+        PlaceVegetableOnShelf(RightBorder, VegetableSprites.Length / 2, VegetableSprites.Length);
+
+    }
+
+    void Shuffle()                                                  // Function to shuffle the array of vegetable sprites
+    {
+        for(int i=0;i<VegetableSprites.Length;i++)
+        {
+            int rand = Random.Range(0, VegetableSprites.Length);
+            tempSprite = VegetableSprites[rand];
+            VegetableSprites[rand] = VegetableSprites[i];
+            VegetableSprites[i] = tempSprite;
+        }
+    }
+
+    void PlaceVegetableOnShelf(GameObject Border,int start, int end)
+    {
+        int counter = 0;
+        for (int i = start; i < end; i++)
+        {
+            GameObject G = Instantiate(Vegetable) as GameObject;
+            G.GetComponent<SpriteRenderer>().sprite = VegetableSprites[i];
+            G.transform.parent = Border.transform;
+            G.transform.localPosition = new Vector3(0f, 0.4f-(counter++*distance),G.transform.localPosition.z);
+        }
+    }
+
+}
--- SaladChefGame/Assets/Scripts/PlayerController.cs
+++ SaladChefGame/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+/* Script to control player movement and actions using Unity's new input system */
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject ChoppingBoard, Plate;
+    public GameObject PlayerChoppingMeter;
+
     Vector2 input_movement;
+    bool pickUpAllowed = false;                                 // true when player trigger is overlapping vegetable/chopping board/plate
+    bool putDownAllowed = false;                                // true when player trigger is overlapping chopping board/plate/trash/customer
+    bool isChopping = false;                                    // true when chopping is taking place
+    string combination;                                         // the combination on the chopboard
+    Sprite pickedVegetableSprite,putDownVegetableSprite;
+    const int maxPickUpAllowed = 2;                             // maximum vegetables player can pick up at once *IMP:if you change this please make corresponding changes to the player prefab children
+    const int maxPutDownAllowed = 6;                            // maximum vegetables that can be placed on the chopping board *IMP:if you change this please make corresponding changes to the choppingboard prefab children
+    int pickedUpCount;                                          // values can be 0-maxPickUpAllowed for number of items the player has currently picked up.
+    int putDownCount;                                           // values can be 0-maxPutDownAllowed for the number of items placed on the chopping board.
 
     [SerializeField]
-    float moveSpeed = 10f;
+    float moveSpeed = 5f;
+    float chopTime  = 5f;
+
 
     private void Update()
     {
-        Move();
+        if(!isChopping) Move();
     }
 
     void Move()
@@ -21,8 +38,94 @@ public class PlayerController : MonoBehaviour
         transform.Translate(movement);
     }
 
-    private void OnMove(InputValue inputValue)  //function to capture input from the new input system
+    private void OnMove(InputValue inputValue)                  //Function to capture input from the new input system
     {
         input_movement = inputValue.Get<Vector2>();
+    }
+
+    private void OnPickUp()
+    {
+        
+        if(pickUpAllowed && !isChopping && pickedUpCount<maxPickUpAllowed)  
+        {
+            transform.GetChild(pickedUpCount).GetComponent<SpriteRenderer>().sprite = pickedVegetableSprite;
+            pickedVegetableSprite = null;
+            if(pickedUpCount<maxPickUpAllowed)  pickedUpCount++;
+        }
+    }
+
+    private void OnPutDown()
+    {
+        if (putDownAllowed && !isChopping && putDownCount < maxPutDownAllowed && pickedUpCount > 0)
+        {
+            
+            ChoppingBoard.transform.GetChild(putDownCount).GetComponent<SpriteRenderer>().sprite = putDownVegetableSprite;
+            gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = putDownVegetableSprite = null;
+            if(putDownCount<maxPutDownAllowed)  putDownCount++;
+            if (pickedUpCount > 0) pickedUpCount--;
+            for(int i = 0;i<pickedUpCount;i++) //rearraning the pickedup sprite on player HUD after the 1st one is kept on the chopping board.
+            {
+                gameObject.transform.GetChild(i).GetComponent<SpriteRenderer>().sprite = gameObject.transform.GetChild(i + 1).GetComponent<SpriteRenderer>().sprite;
+                gameObject.transform.GetChild(i + 1).GetComponent<SpriteRenderer>().sprite = null;
+            }
+            StartCoroutine(StartChopping());
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        #region Pickup trigger stay logic
+        if (other.tag=="Vegetables")
+        {
+            pickUpAllowed = true;
+            pickedVegetableSprite = other.GetComponent<SpriteRenderer>().sprite;
+        }
+
+        #endregion
+
+        #region PutDown trigger stay logic
+
+        if(other.tag==ChoppingBoard.tag)
+        {
+         
+            putDownAllowed = true;
+            putDownVegetableSprite = gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite;
+          
+        }
+
+        #endregion
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        #region Pickup trigger exit logic
+        if (other.tag == "Vegetables")
+        {
+            pickUpAllowed = false;
+        }
+        #endregion
+
+        #region PutDown trigger exit logic
+        if (other.tag == ChoppingBoard.tag)
+        {
+            putDownAllowed = false;
+        }
+        #endregion
+
+    }
+
+    IEnumerator StartChopping()
+    {
+        PlayerChoppingMeter.SetActive(true);
+        float time = chopTime;
+        isChopping = true;
+        while(time>0)
+        {
+            PlayerChoppingMeter.transform.GetChild(0).GetComponent<Image>().fillAmount = 1 - time / chopTime;
+            yield return new WaitForSeconds(0.1f);
+            time-=0.1f;
+        }
+        isChopping = false;
+        PlayerChoppingMeter.SetActive(false);
     }
 }
