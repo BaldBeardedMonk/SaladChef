2df84d7a6601acb05e8b5c4dba4e65acc666b82f
Abhineet <abhineetkrishna@gmail.com>
2020-08-22 22:39:43 +0530

    Implemented customer placement
    Dynamically set and spawn the customer based on the screen size.
    Prefabs and code implemented in a way to make it extensible in the future.
    1. No of customers at once can be modified
    2. Maximum combinations customer can order can be modified

---
+++ SaladChefGame/Assets/Scripts/CustomerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomerController : MonoBehaviour
+{
+    [HideInInspector] public PlaceCustomers pc;
+    [HideInInspector] public int maxSaladItem;      //value set from PlaceCustomer.cs when customer prefab is initialized
+    [HideInInspector] public int pos;               //value set from PlaceCustomer.cs when customer prefab is initialized
+    float waitTimePerVegetable;                     //time in seconds the customer will wait per item in his salad order. Default : 20
+    public List<Sprite> VegetableSprites;
+    public GameObject CustomerTimer;                //timer HUD object
+    [HideInInspector] public float timeToDeduct;    //time to deduct in coroutine. Default is 0.1f
+    void Start()
+    {
+        waitTimePerVegetable = 20f;
+        timeToDeduct = 0.1f;
+        int rand = Random.Range(2, maxSaladItem+1); //selecting number of vegetables the customer demands in the salad. Minimum =2 max = set in PlaceCustomer.cs
+        for(int i=0;i<rand;i++)
+        {
+            gameObject.transform.GetChild(i).gameObject.SetActive(true);
+            int r = Random.Range(0, VegetableSprites.Count);
+            gameObject.transform.GetChild(i).GetComponent<SpriteRenderer>().sprite = VegetableSprites[r];
+            VegetableSprites.RemoveAt(r);
+        }
+        StartCoroutine(StartWaitTimer(rand));
+    }
+
+    IEnumerator StartWaitTimer(int noOfItems)
+    {
+        float waitTime = waitTimePerVegetable * noOfItems;
+        float time = waitTime ;
+        while (time > 0)
+        {
+            CustomerTimer.transform.GetChild(0).GetComponent<Image>().fillAmount = time / waitTime;
+            yield return new WaitForSeconds(timeToDeduct);
+            time -= timeToDeduct;
+        }
+        pc.AddCustomerIndex(pos);   //free up the position for new customer to instansiate.
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ SaladChefGame/Assets/Scripts/PlaceCustomers.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*This script handles placement of the customers dynamically based on the screen width and the maximum number of vegetable combination each customer can demand
+  maxSaladItem : can be set from inspector
+  maxCustomerPossible: is calculated based on the width of the screen/space required for individual customer
+  customerPositions: is Vector2 which holds individual positions where customer can be placed. Again dynamically calculated based on screen size and max customer possible
+  customerOccupiedIndex : is a list of integers which holds all available position to spawn the customer. When customer is spawned the position is removed from the list
+                          and when customer is destroyed the position is added back into the list.*/
+
+public class PlaceCustomers : MonoBehaviour
+{
+    public GameObject BackgroundSprite,Customer;
+    [SerializeField]
+    [Range(2,6)]
+    public int maxSaladItem =3;                     //maximum number of combinations that a customer can order. Default is 3. Maximum is 6. To add more make changes to customer prefab
+    int maxCustomersPossible;                       //maximum number of customers that can fit the screen. Also depends on the maximum combination a customer can have.
+    int startPositionX, startPositionY;
+    Vector2 [] customerPositions;
+    [HideInInspector] public List<int> customerOccupiedIndex;   //the values in the list will correspond to free index positions
+    
+    float customerSpawnTime;                        //time in seconds before a new customer is spawned.
+
+    void Start()
+    {
+        startPositionX = (int)Mathf.Round(BackgroundSprite.transform.localScale.x);
+        startPositionY = (int)Mathf.Round(BackgroundSprite.transform.localScale.y);
+        maxCustomersPossible = ((startPositionX*2)/ maxSaladItem) +1;
+        customerOccupiedIndex = new List<int>(maxCustomersPossible);
+        customerPositions = new Vector2[maxCustomersPossible];
+        customerSpawnTime = 50f;
+
+        for(int i=0; i<maxCustomersPossible; i++)
+        {
+            customerPositions[i] = new Vector2(-startPositionX + (i * maxSaladItem), startPositionY);
+            customerOccupiedIndex.Add(i);       //initially the values will index of be all the free positions available.
+        }
+
+        /*We start by placing two customers. One at the left of the screen and one at the right of the screen. After that there will be a new customer spawned every 50 seconds
+         * or when a customer is served or his timer runs out */  
+        int r = Random.Range(0, customerOccupiedIndex.Count / 2);
+        int pos = customerOccupiedIndex[r];
+        PlaceCustomer(customerPositions[pos], pos);
+        customerOccupiedIndex.Remove(pos);     //remove the index value from the list as this position is already occupied by a customer  
+        r = Random.Range(customerOccupiedIndex.Count / 2, customerOccupiedIndex.Count);
+        pos = customerOccupiedIndex[r];
+        PlaceCustomer(customerPositions[pos], pos);
+        customerOccupiedIndex.Remove(pos);      //remove the index value from the list as this position is already occupied by a customer
+        InvokeRepeating("CallPlaceCustomer", customerSpawnTime, customerSpawnTime);
+
+    }
+
+    void PlaceCustomer(Vector2 customerPosition,int pos)
+    {
+        GameObject C = Instantiate(Customer, customerPosition, Quaternion.identity) as GameObject;
+        C.GetComponent<CustomerController>().maxSaladItem = maxSaladItem;
+        C.GetComponent<CustomerController>().pos = pos;
+        C.GetComponent<CustomerController>().pc = gameObject.GetComponent<PlaceCustomers>();
+    }
+
+    public void AddCustomerIndex(int pos)
+    {
+        customerOccupiedIndex.Add(pos);
+        Invoke("CallPlaceCustomer", 5f);
+        
+    }
+
+    void CallPlaceCustomer()
+    {
+        if (customerOccupiedIndex.Count>0)
+        {
+            int r = Random.Range(0, customerOccupiedIndex.Count);
+            int pos = customerOccupiedIndex[r];
+            PlaceCustomer(customerPositions[pos], pos);
+            customerOccupiedIndex.Remove(pos);  //remove the index value from the list as this position is already occupied by a customer
+        }
+    }
+
+
+}
